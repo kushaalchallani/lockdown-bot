083267fdcd25527cf1b8d74bb02bbffc11af3d44
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2021-06-23 14:47:48 +0530

    solved problem in args

--- src/commands/lockdown.js
+++ src/commands/lockdown.js
@@ -11,48 +11,74 @@ module.exports = class extends Command {
     }
 
     async execute(message, args) {
-    message.delete();
+        message.delete();
 
-     if(!message.member.hasPermission("ADMINISTATOR")) return;
-     const lockdownReason = args[0];
-     const ignored = new Set(["853469341646716960", "853469367865573386", "853317838168391690", "853469442898001920", // staff channels
-    "854595915583193109", "854596020218495046", "854695067347058688", "856867360026787870",// logs
-    "857139930068287489",
-]); 
-     const serverChannels = message.guild.channels.cache.filter(ch => ch.type !== "category");
-     if(!lockdownReason) return message.channel.send("Provide a reason to the channel");
+        if (!message.member.hasPermission("ADMINISTATOR")) return;
+        const lockdownReason = args.join(" ");
+        const ignored = new Set([
+            "853469341646716960",
+            "853469367865573386",
+            "853317838168391690",
+            "853469442898001920", // staff channels
+            "854595915583193109",
+            "854596020218495046",
+            "854695067347058688",
+            "856867360026787870", // logs
+            "857139930068287489",
+        ]);
+        const serverChannels = message.guild.channels.cache.filter((ch) => ch.type !== "category");
+        if (!lockdownReason) return message.channel.send("Provide a reason to the channel");
 
-     serverChannels.forEach(channel => {
-         if(!ignored.has(channel.id)) {
-             if(channel.type === "text") {
-                channel.updateOverwrite(channel.guild.roles.cache.get("853323459098443776"), { VIEW_CHANNEL: false });
-                // channel.send("<a:read:746004477772955718> This server is under lockdown visit <#732903985047928842> for moreinfo");
-             } else if(channel.type === "voice") {
-                channel.updateOverwrite(channel.guild.roles.cache.get("853323459098443776"), { VIEW_CHANNEL: false });
-             } else if(channel.type === "news") {
-                channel.updateOverwrite(channel.guild.roles.cache.get("853323459098443776"), { VIEW_CHANNEL: false });
-             } 
-         }
-     });
+        serverChannels.forEach((channel) => {
+            if (!ignored.has(channel.id)) {
+                if (channel.type === "text") {
+                    channel.updateOverwrite(channel.guild.roles.cache.get("853323459098443776"), {
+                        VIEW_CHANNEL: false,
+                    });
+                    // channel.send("<a:read:746004477772955718> This server is under lockdown visit <#732903985047928842> for moreinfo");
+                } else if (channel.type === "voice") {
+                    channel.updateOverwrite(channel.guild.roles.cache.get("853323459098443776"), {
+                        VIEW_CHANNEL: false,
+                    });
+                } else if (channel.type === "news") {
+                    channel.updateOverwrite(channel.guild.roles.cache.get("853323459098443776"), {
+                        VIEW_CHANNEL: false,
+                    });
+                }
+            }
+        });
 
-     message.guild.channels.cache.get("857139930068287489") .updateOverwrite(message.guild.roles.cache.get("853323459098443776"), { VIEW_CHANNEL: true, SEND_MESSAGES: false });
-     
-     const purgechannel = message.guild.channels.cache.get("857139930068287489");
-     purgechannel.bulkDelete(100);
-     
-    const currentdate = new Date();
-    const datetime = currentdate.getDate() + "/"
-    + (currentdate.getMonth() + 1) + "/"
-    + currentdate.getFullYear() + " at "
-    + currentdate.getHours() + ":"
-    + currentdate.getMinutes() + ":"
-    + currentdate.getSeconds();
+        message.guild.channels.cache
+            .get("857139930068287489")
+            .updateOverwrite(message.guild.roles.cache.get("853323459098443776"), {
+                VIEW_CHANNEL: true,
+                SEND_MESSAGES: false,
+            });
 
-     const lockmessage = new Embed()
-     .setColor("#0099ff")
-     .setAuthor("Server locked", "https://cdn.discordapp.com/emojis/752490207886049431.gif")
-     .setDescription(`<a:codered:857150846215389194>  The server is in lockdown.\n\n\n Reason: **${lockdownReason}** `)
-     .setFooter(datetime);
-    message.guild.channels.cache.find(ch => ch.name === "🔒┃lockdown").send(lockmessage);
+        const purgechannel = message.guild.channels.cache.get("857139930068287489");
+        purgechannel.bulkDelete(100);
+
+        const currentdate = new Date();
+        const datetime =
+            currentdate.getDate() +
+            "/" +
+            (currentdate.getMonth() + 1) +
+            "/" +
+            currentdate.getFullYear() +
+            " at " +
+            currentdate.getHours() +
+            ":" +
+            currentdate.getMinutes() +
+            ":" +
+            currentdate.getSeconds();
+
+        const lockmessage = new Embed()
+            .setColor("#0099ff")
+            .setAuthor("Server locked", "https://cdn.discordapp.com/emojis/752490207886049431.gif")
+            .setDescription(
+                `<a:codered:857150846215389194>  The server is in lockdown.\n\n\n Reason: **${lockdownReason}** `
+            )
+            .setFooter(datetime);
+        message.guild.channels.cache.find((ch) => ch.name === "🔒┃lockdown").send(lockmessage);
     }
 };
